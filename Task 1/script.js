25cf58f4146c754c043733df8b098bf8a3724111
GedasIncirauskas <gedas.inc@gmail.com>
2021-07-27 19:05:05 +0300

    done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,23 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const formValidation = document
+  .querySelector("form")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = e.target.elements[0].value;
+    calculateValues(inputValue);
+  });
+
+function calculateValues(values) {
+  const lb = (values * 2.2046).toFixed(2);
+  const g = (values / 0.001).toFixed(2);
+  const oz = (values * 35.274).toFixed(2);
+  const selectOutPut = document.getElementById("output");
+  const p = document.createElement("p");
+  p.style.color = "rgb(255,255,255)";
+  p.style.fontSize = "2rem";
+  p.textContent = `Svarai: ${lb} Gramai: ${g} Uncijos: ${oz}`;
+  selectOutPut.appendChild(p);
+}
