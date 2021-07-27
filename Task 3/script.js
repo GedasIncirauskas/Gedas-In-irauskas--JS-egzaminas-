189df8b259a7f415efbd682cc9f5510ea2320e30
GedasIncirauskas <gedas.inc@gmail.com>
2021-07-27 20:24:57 +0300

    done

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,34 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("btn").addEventListener("click", () => {
+      createUsertList(data);
+    });
+  })
+  .catch((err) => console.log(err));
+
+function createUsertList(data) {
+  const showUsertText = document.getElementById("message");
+  const output = document.getElementById("output");
+  if (!showUsertText.lentgh > 0) {
+    showUsertText.textContent = "";
+  }
+  data.forEach((item) => {
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("list-style");
+    output.appendChild(contentDiv);
+    const p = document.createElement("p");
+    p.textContent = `Username: ${item.login}`;
+    const userImg = document.createElement("img");
+    userImg.src = `${item.avatar_url}`;
+    userImg.alt = `${item.login}`;
+    userImg.style.width = "10rem";
+    contentDiv.appendChild(p);
+    contentDiv.appendChild(userImg);
+  });
+}
