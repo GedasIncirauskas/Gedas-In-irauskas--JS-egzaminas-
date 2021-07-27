91da165ffc081bb693e0d199b592f0b6eacbd7ac
GedasIncirauskas <gedas.inc@gmail.com>
2021-07-27 22:56:26 +0300

    done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(num1, num2) {
+  this.num1 = num1;
+  this.num2 = num2;
+  this.sum = () => {
+    return num1 + num2;
+  };
+  this.subtraction = () => {
+    return num1 - num2;
+  };
+  this.multiplication = () => {
+    return num1 * num2;
+  };
+  this.division = () => {
+    return num1 / num2;
+  };
+}
+
+const number = new Calculator(16, 5);
+console.log(number);
+console.log(number.sum());
+console.log(number.subtraction());
+console.log(number.multiplication());
+console.log(number.division());
