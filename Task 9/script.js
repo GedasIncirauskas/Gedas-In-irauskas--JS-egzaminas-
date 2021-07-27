59d146cde2e7945e92a0d914ad60b3ada8a1d62f
GedasIncirauskas <gedas.inc@gmail.com>
2021-07-27 22:36:49 +0300

    done

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,18 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    this.budget >= 100000000 ? console.log(true) : console.log(false);
+  }
+}
+
+const movie = new Movie("Filmas", "Filmauskas", 200000000);
+console.log(movie);
+movie.wasExpensive();
